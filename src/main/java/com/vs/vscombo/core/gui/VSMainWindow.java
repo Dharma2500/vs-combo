da6b765348aa8f7538cebfd5c934dfa6e3331e16
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-05 14:41:10 +0500

    Update VSMainWindow.java

--- src/main/java/com/vs/vscombo/core/gui/VSMainWindow.java
+++ src/main/java/com/vs/vscombo/core/gui/VSMainWindow.java
@@ -5,21 +5,19 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.MathHelper;
 
 public class VSMainWindow extends Screen {
     
     private static VSMainWindow instance;
     private static boolean isOpen = false;
     
-    // Layout Constants
     private static final int BG_ALPHA = 180;
     private static final float SCREEN_RATIO = 0.25f;
-    private static final int PANEL_COLOR = 0xFF1A1A1A; // Dark strict background
+    private static final int PANEL_COLOR = 0xFF1A1A1A;
     private static final int TEXT_COLOR = 0xFFE0E0E0;
     
-    // Components
     private TabManager tabManager;
 
     private VSMainWindow() {
@@ -41,7 +39,6 @@ public class VSMainWindow extends Screen {
 
     @Override
     protected void init() {
-        // Calculate 25% screen dimensions, centered
         int winW = this.width;
         int winH = this.height;
         int panelW = (int)(winW * SCREEN_RATIO);
@@ -49,10 +46,8 @@ public class VSMainWindow extends Screen {
         int posX = (winW - panelW) / 2;
         int posY = (winH - panelH) / 2;
         
-        // Initialize Tab System (Left sidebar)
         this.tabManager.init(posX, posY, panelW, panelH);
         
-        // Add Tab Buttons
         this.addButton(new Button(
             posX + 5, posY + 25, 100, 20, 
             new StringTextComponent("Macros#1"), 
@@ -62,12 +57,10 @@ public class VSMainWindow extends Screen {
 
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-        // 1. Render Dark Overlay (Dim background)
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         fill(matrixStack, 0, 0, this.width, this.height, (BG_ALPHA << 24) | 0x000000);
         
-        // 2. Render Main Panel
         int winW = this.width;
         int winH = this.height;
         int panelW = (int)(winW * SCREEN_RATIO);
@@ -75,17 +68,13 @@ public class VSMainWindow extends Screen {
         int posX = (winW - panelW) / 2;
         int posY = (winH - panelH) / 2;
         
-        // Panel Background
         fill(matrixStack, posX, posY, posX + panelW, posY + panelH, PANEL_COLOR);
-        // Border
         drawHorizontalLine(matrixStack, posX, posX + panelW, posY, 0xFF555555);
         drawVerticalLine(matrixStack, posX, posY, posY + panelH, 0xFF555555);
         
-        // 3. Render Header
         drawString(matrixStack, this.font, "Created by Vitaly_Sokolov", 
                 posX + 10, posY + 8, TEXT_COLOR);
         
-        // 4. Render Active Tab Content
         if (this.tabManager.getActiveTab() != null) {
             this.tabManager.getActiveTab().render(matrixStack, mouseX, mouseY, partialTicks, 
                     posX + 120, posY + 25, panelW - 130, panelH - 35);
@@ -96,10 +85,9 @@ public class VSMainWindow extends Screen {
     
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        // Intercept input for active tab (e.g. Text Editor)
         if (this.tabManager.getActiveTab() != null) {
             if (this.tabManager.getActiveTab().keyPressed(keyCode, scanCode, modifiers)) {
-                return true; // Consumed by tab
+                return true;
             }
         }
         return super.keyPressed(keyCode, scanCode, modifiers);
