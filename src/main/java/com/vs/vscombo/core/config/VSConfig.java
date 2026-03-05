aa7c449ff4d8372f95132f511cee418bcc73c22c
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-05 14:29:55 +0500

    Create VSConfig.java

---
+++ src/main/java/com/vs/vscombo/core/config/VSConfig.java
@@ -0,0 +1,25 @@
+package com.vs.vscombo.core.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class VSConfig {
+    
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+    
+    public static final ForgeConfigSpec.ConfigValue<String> MAIN_WINDOW_KEY;
+    
+    static {
+        BUILDER.push("interface");
+        MAIN_WINDOW_KEY = BUILDER
+                .comment("Default key to open VS Universe (Virtual Key Code)")
+                .define("mainWindowKey", "X");
+        BUILDER.pop();
+        
+        SPEC = BUILDER.build();
+    }
+    
+    public static void load() {
+        // Config loading logic handled by Forge auto-sync
+    }
+}
