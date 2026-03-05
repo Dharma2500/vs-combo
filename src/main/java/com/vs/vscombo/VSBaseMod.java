dc1c48f3536ad8d7aab0f873d3d279b669bf301f
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-05 14:39:27 +0500

    Update VSBaseMod.java

--- src/main/java/com/vs/vscombo/VSBaseMod.java
+++ src/main/java/com/vs/vscombo/VSBaseMod.java
@@ -1,27 +1,21 @@
 package com.vs.vscombo;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod("vscombo")
 public class VSBaseMod {
     public static final String MOD_ID = "vscombo";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
     
-    // Centralized Registry Holder
-    public static final DeferredRegistry REGISTRY = new DeferredRegistry();
-
     public VSBaseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
-        // Initialize VS Core components
-        REGISTRY.register(modEventBus);
-        
+        com.vs.vscombo.common.registry.DeferredRegistry.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
-        LOGGER.info("VS Base initialized. Architecture: Secure. Side: Client.");
+        LOGGER.info("VS Base initialized. Forge 1.16.5 compatible.");
     }
 }
